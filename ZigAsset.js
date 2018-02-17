5e2d61ed1793f11bb6e062793c214728e77e1afe
Jeff Fowler <jeffowler@gmail.com>
2018-02-17 23:59:16 +0100

    loader

--- ZigAsset.js
+++ ZigAsset.js
@@ -1,9 +1,19 @@
-const {Asset} = require('parcel-bundler');
+const path = require('path');
+const { Asset, md5 } = require('parcel-bundler');
+const { exec } = require('child_process');
 
 class ZigAsset extends Asset {
+  async parse() {
+    this.wasmPath = path.join(this.options.cacheDir, this.name + '.wasm');
+    const cmd = `zig build-obj ${this.name} --output ${this.wasmPath}`
+    const x = exec(cmd)
+    x.stderr.pipe(process.stderr)
+    x.stdout.pipe(process.stdout)
+  }
+
   async generate() {
     return {
-      js: "module.exports='this is a thing'",
+      js: `module.exports='${this.name}fjijij'`
     };
   }
 }
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function (bundler) {
   bundler.addAssetType('zig', require.resolve('./ZigAsset'));
-}
+};
